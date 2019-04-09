20c36ee241fc4910cbde4f2ee09a62a5558320e9
Stuart Green <stuartngreen@gmail.com>
2019-04-09 14:06:11 +0200

    added exercise 2

--- eloquent-javascript/chapter-3/exercise-2.js
+++ eloquent-javascript/chapter-3/exercise-2.js
@@ -3,14 +3,18 @@ function recursion(num) {
     let output = '';
 
     function isEven(num) {
-        if (num == 0) return true;
-        else if (num == 1) return false;
-        else if (num < 0) return isEven(-n);
-        else return isEven(num - 2);
+
+        if (num == 0)
+            return true;
+        else if (num == 1)
+            return false;
+        else if (num < 0)
+            return isEven(-num);
+        else 
+            return isEven(num - 2);
     }
 
     output += isEven(num);
-
+    // console.log(output);
     document.getElementById('output').innerHTML = 'Even number? ' + output;
-    
 }
