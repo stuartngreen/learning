59869a6119fc9c539ef14d115c7d61e4cd5cad11
Stuart Green <stuartngreen@gmail.com>
2019-04-16 09:54:38 +0200

    exercise 1 for chapter 4

--- eloquent-javascript/chapter-4/exercise-1.js
+++ eloquent-javascript/chapter-4/exercise-1.js
@@ -1,6 +1,40 @@
-function callSumRange() {
+function range(start, end) {
+    
+    let range = [];
 
-    let output = 'callSumRange()';
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for (start; start <= end; start++) {
+        range.push(start);
+    }
+
+    return range;
+}
+
+function sum(numbers) {
+
+    let total = 0;
+
+    if (numbers.length) {
+
+        for (let i = 0; i < numbers.length; i++)  {
+            total += numbers[i];
+        }
+
+        return total;
+    }
+    else {
+        console.log('Function requires an array of numbers');
+    }
+}
+
+function callSumRange(start, end) {
+
+    let output = `The sum of the range of numbers from ${ start } to ${ end } is ${ sum(range(start, end)) }.`;
 
     document.getElementById('output').innerHTML = output;
 }
