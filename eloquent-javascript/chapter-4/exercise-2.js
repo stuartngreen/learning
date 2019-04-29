7184b29057a3299f4c9cc620b2c6d461b5d33295
Stuart Green <stuartngreen@gmail.com>
2019-04-29 11:47:52 +0200

    added exercise 2/3

--- eloquent-javascript/chapter-4/exercise-2.js
+++ eloquent-javascript/chapter-4/exercise-2.js
@@ -1,6 +1,41 @@
-function callReverseArray() {
+function reverseArray(arr) {
+	
+	if (arr.length) {
+		let newArr = [];
+		
+		for (let i = arr.length - 1; i >= 0; i--) {
+			newArr.push(arr[i]);
+		}
+		
+		return newArr;
+	}
+	else {
+		console.log('Function requires an array as input.');
+	}
+}
 
-    let output = 'callReverseArray()';
+function reverseArrayInPlace(arr) {
+	
+	if (arr.length) {
+		let i = arr.length - 1;
+		
+		for (let j = 0; j < Math.floor(arr.length / 2); j++) {
+			let temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+			i--;
+		}
+		
+		return arr;
+	}
+	else {
+		console.log('Function requires an array as input.');
+	}
+}
+
+function callReverseArray(arr) {
+	
+    let output = `New reversed array:\t\t ${ reverseArray(arr) }\nOriginal array reversed:\t ${ reverseArrayInPlace(arr) }`;
 
     document.getElementById('output').innerHTML = output;
 }
--- eloquent-javascript/chapter-4/exercise-3.js
+++ eloquent-javascript/chapter-4/exercise-3.js
@@ -1,6 +1,69 @@
-function callArrayToList() {
+function arrayToList(arr) {
+	let list = null;
+	
+	for (let i = arr.length - 1; i >= 0; i--) {
+		list = {
+			value: arr[i],
+			link: list
+		};
+	}
+	
+	return list;
+}
 
-    let output = 'callArrayToList()';
+function listToArray(list) {
+	let arr = [];
+	
+	for (let node = list; node !== null; node = node.link) {
+		arr.push(node.value);
+	}
+	
+	return arr;
+}
 
-    document.getElementById('output').innerHTML = output;
+function prepend(el, list) {
+	let newList = null;
+
+	newList = {
+		value: el,
+		link: list
+	}
+
+	return newList;
+}
+
+function nth(list, num) {
+	let node = list;
+
+	for (let i = 0; i < num; i++) {
+		node = node.link;
+	}
+
+	return node ? node.value : undefined;
+}
+
+function nthRecursive(list, num) {
+	if (!list) {
+		return undefined;
+	}
+	else if (num === 0) {
+		return list.value;
+	}
+	else {
+		list = list.link;
+		return nthRecursive(list, num - 1);
+	}
+}
+
+function callArrayToListToArray(array, el, pos) {
+
+	let list = arrayToList(array);
+	let output1 = `Converted to list:\t ${ JSON.stringify(list) } \nConverted back to array:\t ${ listToArray(list) }`;
+
+	let newList = prepend(el, list);
+	let output2 = `Prepend element "${ el }" to list:\t ${ JSON.stringify(newList) } \nConverted back to array:\t ${ listToArray(newList) }`;
+
+	let output3 = `The element at node ${ pos } in the list is "${ nthRecursive(newList, pos) }"`;
+
+	document.getElementById('output').innerHTML = output1 + '\n\n' + output2 + '\n\n' + output3;
 }
