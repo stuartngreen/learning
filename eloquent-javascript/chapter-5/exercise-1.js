c83ec3f0dd279367a00b2451fef6285169341acb
Stuart Green <stuartngreen@gmail.com>
2019-04-30 11:10:15 +0200

    hiding chapter 5 until I get more skillzzzz

--- eloquent-javascript/chapter-5/exercise-1.js
+++ eloquent-javascript/chapter-5/exercise-1.js
@@ -6,3 +6,17 @@ function callFlattening() {
 
     document.getElementById('output').innerHTML = output;
 }
+
+
+
+
+
+
+
+
+let array = [];
+array.push([1,2,3], [4,5,6], [7,8,9]);
+
+console.log(array.reduce((flat, current) => flat.concat(current), []));
+
+
